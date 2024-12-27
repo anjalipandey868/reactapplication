fd271052cd696c3264fc71d4a240c36030ef431c
unknown <anjalipandey868@gmail.com>
2024-12-27 19:04:16 +0530

    episode2

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-//nested htm structure in react
-
+//nested html structure in react
+import React from "react"
+import ReactDOM from "react-dom/client"
 const heading = React.createElement("div",{id: "parent" , xyz:"abc"},
     React.createElement("div",{id: "child" }, 
         React.createElement("h1",{id: "heading" },"I am h1 tag" )
     ))
     const root = ReactDOM.createRoot(document.getElementById("root"))
     root.render(heading)
-    console.log(heading)
